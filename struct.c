6e76a5366762d6ccff1e93ee35ed37abddd97f21
iidrn <idrni@icloud.com>
2023-04-17 14:23:38 +0300

    simple cats array

--- struct.c
+++ struct.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+// typedef
+
 int main(void) {
-    printf("Hello dad, watch me code!\n");
+    char *myCats[] = {"Cookie", "Luna", "Lucy", "Lotus", "Coconut", "Father of 20"};
+    size_t catsCount = sizeof(myCats) / sizeof(myCats[0]);
+
+    printf("You had %zu cats!\n", catsCount);
+
+    for (int i = 0; i < catsCount; i++)
+    {
+        printf("%s\n", myCats[i]);
+    }
 }
