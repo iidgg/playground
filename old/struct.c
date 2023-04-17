ce677a33751008301c87ec94e7a346b5925baf6e
iidrn <idrni@icloud.com>
2023-04-17 14:40:14 +0300

    explain issue

--- old/struct.c
+++ old/struct.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Now the issue here is you can't extend the array dynamicly/easily
+
 int main(void) {
     char *myCats[] = {"Cookie", "Luna", "Lucy", "Lotus", "Coconut", "Father of 20"};
     size_t catsCount = sizeof(myCats) / sizeof(myCats[0]); // counts the array size "mathematically"
