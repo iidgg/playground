cebc10a058be444a1cf3212a4d1aab2d6e18b0e0
id <105822145+iidgg@users.noreply.github.com>
2023-04-23 02:32:16 +0300

    Update args.py

--- args.py
+++ args.py
@@ -1,5 +1,11 @@
 # a cs50 watching the lecture while coding / 2023 week -6 python
-from sys import argv
+import sys
 
-for arg in argv[1:]: # the [1:] makes the array start from index 1
+if len(sys.argv) <= 1:
+    print("Missing command-line argument")
+    sys.exit(1)
+
+for arg in sys.argv[1:]: # the [1:] makes the array start from index 1
     print(arg)
+
+sys.exit(0)
