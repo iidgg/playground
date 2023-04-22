e4439ef13babbf42bb0410707a70f99003ad1666
id <105822145+iidgg@users.noreply.github.com>
2023-04-23 01:53:39 +0300

    Create mario.py

---
+++ mario.py
@@ -0,0 +1,36 @@
+# a cs50 watching the lecture while writing / 2023 week -6 python
+def main():
+    typo = getTypo()
+    length = getLength()
+
+    for i in range(length):
+        if typo == "V":
+            print("#")
+        elif typo in ["H", "B"]:
+            print("#" * length)
+
+    print()
+
+
+def getLength():
+    while True:
+        try:
+            n = int(input("Length: "))
+            if n > 0:
+                return n
+        except ValueError:
+            print("bruh, i need a NUMBER!")
+
+
+def getTypo():
+    while True:
+        try:
+            t = input('Type? (Vertical "v"/Horizontal "h"/Both "b"): ').upper()
+
+            if t in ["V", "H", "B"]:
+                return t
+        except:
+            print("Something went wrong try again sweetie.")
+
+
+main()
